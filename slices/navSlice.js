336c18680b498d41cfd7a93023001b0b3c8a8bdf
NityaAgarwal18 <nityaagarwal0218@gmail.com>
2024-04-07 12:54:42 +0530

    created nav slice

---
+++ slices/navSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+
+export const { setDestination, setOrigin, setTravelTimeInformation } =
+  navSlice.actions;
+
+// selectors
+
+export const selectOrigin = (state) => state.action.origin;
+export const selectDestination = (state) => state.action.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.action.travelTimeInformation;
+
+export default navSlice.reducer;
