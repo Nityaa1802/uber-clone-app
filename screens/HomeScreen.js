711a4134634e9287cef61ccad2d9eec0590c9da1
NityaAgarwal18 <nityaagarwal0218@gmail.com>
2024-04-08 13:55:07 +0530

    added google places autocomple search box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,14 +2,39 @@ import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`flex-1`}>
       <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: 'contain' }}
           source={{ uri: 'https://links.papareact.com/gzs' }}
+        />
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+          placeholder='Where form?'
+          styles={{ container: { flex: 0 }, textInput: { fontSize: 10 } }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails
         />
         <NavOptions />
       </View>
